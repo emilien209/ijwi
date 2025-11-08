24042b2002452c088d920ca66adcc9d96bc3871d
ndishimye emilien <ndishimyeemilien29@gmail.com>
2025-11-08 08:05:38 +0000

    koresha ino firebase kandi ikurikize amakuruyayo {// Import the function

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -7,7 +7,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyAN95KDrL7_VbM6DA5jnanYYHXZLLDi6NQ",
   authDomain: "tora-b66a1.firebaseapp.com",
   projectId: "tora-b66a1",
-  storageBucket: "tora-b66a1.appspot.com",
+  storageBucket: "tora-b66a1.firebasestorage.app",
   messagingSenderId: "474621598483",
   appId: "1:474621598483:web:864347e5d5e570d179d8e6",
   measurementId: "G-GX4G4LNMVG"
